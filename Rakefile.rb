6c9ee58057ee96e781763cd957ceee9905e99300
Henrik <henrik@haf.se>
2012-03-23 15:46:41 +0100

    adding nlog as a dependency

--- Rakefile.rb
+++ Rakefile.rb
@@ -63,6 +63,7 @@ nuspec :nr_nuspec do |nuspec|
   nuspec.licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0" # TODO: set this for nuget generation
   nuspec.requireLicenseAcceptance = "false"
   nuspec.dependency "RabbitMQ.Client", "2.8.1"
+  nuspec.dependency "NLog", "2.0.0"
   nuspec.output_file = FILES[:nr][:nuspec]
   nuspec_copy(:nr, "#{PROJECTS[:nr][:id]}.{dll,xml}")
 end
